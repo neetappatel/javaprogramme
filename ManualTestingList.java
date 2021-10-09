1c91cfb0924a3d124e466726baa2b86a00a050aa
Neeta patel <neetapatel2505@gmail.com>
2021-10-09 14:34:14 +0100

    this is my first commit

---
+++ ManualTestingList.java
@@ -0,0 +1,50 @@
+package HomeWork;
+
+public class ManualTestingList {
+
+    //Main method to execute the program
+    public static void main(String[] args)
+        System.out.println("Learnig git")
+
+        System.out.println("What is Software");
+        System.out.println("What is Software Testing");
+        System.out.println("Benefit of Software Testing");
+        System.out.println("Who does Testing");
+        System.out.println("Qualification of a Tester");
+        System.out.println("What is Manual Testing");
+        System.out.println("Disadvantage of Manual Testing");
+        System.out.println("SDLC");
+        System.out.println("STLC");
+        System.out.println("Test Plan and it's Benefits");
+        System.out.println("Test plan Template");
+        System.out.println("Test Scenario + Template");
+        System.out.println("Test Case + Template");
+        System.out.println("Test Scenario vs Test Case");
+        System.out.println("Testing Techniques");
+        System.out.println("What is Bug");
+        System.out.println("Bug Life Cycle");
+        System.out.println("Bug Template");
+        System.out.println("Severity vs Priority");
+        System.out.println("Testing Levels");
+        System.out.println("White Box Testing");
+        System.out.println("Black Box Testing");
+        System.out.println("Static vs Dynamic Testing");
+        System.out.println("Exploratory Testing");
+        System.out.println("Ad-Hoc Testing");
+        System.out.println("Software Development Models");
+        System.out.println("Waterfall");
+        System.out.println("Iterative");
+        System.out.println("V-Models");
+        System.out.println("Verification and Validation");
+        System.out.println("Software Development Models");
+        System.out.println("Agile");
+        System.out.println("Definition of Ready/Done");
+        System.out.println("TDD VS BDD");
+
+
+
+
+
+    }
+
+}
---
+++ MyName.java
@@ -0,0 +1,12 @@
+package HomeWork;
+
+public class MyName {
+
+    //Main method to execute the proram
+    public static void main(String[] args){
+
+        // printName ane surname in saperate line
+        System.out.print("Neeta");
+        System.out.print(" Patel");
+    }
+}
